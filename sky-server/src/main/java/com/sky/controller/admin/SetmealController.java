037f347aa1a8b1be35cc96c1a332b65f12a90f08
yjp <1773702740@qq.com>
2025-08-14 14:35:39 +0800

    起售停售套餐业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -87,4 +87,12 @@ public class SetmealController {
         setmealService.update(setmealDTO);
         return Result.success();
     }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用或停用套餐")
+    public Result StratOrStop( @PathVariable Integer status, Long id) {
+        log.info("启用或停用套餐");
+        setmealService.startOrStop( status, id);
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -40,4 +40,11 @@ public interface SetmealService {
      * @return
      */
     SetmealVO getByIdWithDish(Long id);
+
+    /**
+     * 套餐起售、停售
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
